3f13f6ee3c0a2dd3ac9f866957a0f66eea8bc478
Scott Graham <scttgrhm7+public@gmail.com>
2023-11-22 09:30:19 -0500

    Add Docker

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import Button from "./Button";
 import { fetchData } from "./services/api";
 
 function App() {
-  const [data, setData] = useState<any>(null);
-
   useEffect(() => {
     const fetchDataFromApi = async () => {
       try {
         const result = await fetchData("/"); // Endpoint on your Express server
         console.log({ result });
-        setData(result);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
